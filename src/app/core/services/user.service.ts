6e454b13064011c5f4768c1ec465893f5bef6b31
Rafagar123 <rafagarrido17@gmail.com>
2026-05-12 19:03:52 +0200

    feat: lista usuarios

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,28 +1,54 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { Injectable, numberAttribute } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Usuario } from '../models/user.model';
 import to from "./utils.service";
+import ConstUrls from 'src/app/shared/contants/const-urls';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  async obtenerUsuarioPorId(id: number) {
+  /*async obtenerUsuarioPorId(id: number) {
     return await to(
-        this.http
-            .get<Usuario>('/assets/mocks/user.json')
-            .toPromise()
+      this.http
+        .get<Usuario>()
+        .toPromise()
+    )
+  }
+*/
+  async obtenerUsuarios(nickUsuario: string, contrasena: string) {
+    let params = new HttpParams()
+      .set(ConstUrls.NICK_USUARIO_PARAM, nickUsuario)
+      .set(ConstUrls.PASS_USUARIO_PARAM, contrasena);
+    return await to(
+      this.http
+        .get<any>(
+          `${ConstUrls.API_URL}/api/v1/usuarios/usuarios`,
+          {
+            params: params
+          }
+        )
+        .toPromise()
     )
   }
 
-  async obtenerUsuarios() {
+  async obtenerDireccionPrincipal(usuarioId: number, nickUsuario: string, contrasena: string) {
+    let params = new HttpParams()
+      .set(ConstUrls.NICK_USUARIO_PARAM, nickUsuario)
+      .set(ConstUrls.PASS_USUARIO_PARAM, contrasena);
     return await to(
-        this.http
-            .get<Usuario>('/assets/mocks/user.json')
-            .toPromise()
+      this.http
+        .get<any>(
+          `${ConstUrls.API_URL}/api/v1/direcciones/direc-por-usuario/${numberAttribute}`,
+          {
+            params: params
+          }
+        )
+        .toPromise()
     )
   }
 
--- src/app/features/user-list/user-list.component.ts
+++ src/app/features/user-list/user-list.component.ts
@@ -1,26 +1,83 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import {Router} from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import { Router } from "@angular/router";
 import { UserPopupComponent } from '../user-popup/user-popup.component';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
   standalone: true,
-  imports: [ CommonModule, UserPopupComponent ]
+  imports: [CommonModule, UserPopupComponent, FormsModule]
 })
 export class UserListComponent implements OnInit {
   @Output() cerrarPopUpOk = new EventEmitter<void>();
   @Output() cerrarPopUpCancel = new EventEmitter<void>();
 
+  userService: UserService;
+  nickUsuario: string = '';
+  contrasena: string = '';
+  usuarios: any[] = [];
+  direcciones: any[] = [];
+
+
   modoPopup: String = 'CLOSED';
 
-  constructor(private router: Router) {
-
+  constructor(private router: Router, userService: UserService) {
+    this.userService = userService;
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+
+    const nickUsuario = localStorage.getItem('nickUsuario') || '';
+    const contrasena = localStorage.getItem('contrasena') || '';
+
+    try {
+      const response = await this.userService.obtenerUsuarios(nickUsuario, contrasena);
+
+      console.log('Usuarios:', response);
+
+      this.usuarios = response;
+
+    } catch (error) {
+      console.error('Error real:', error);
+    }
+  }
+
+  async obtenerDireccionPrincipal(usuarioId: number): Promise<string | null> {
+
+    const nickUsuario = localStorage.getItem('nickUsuario') || '';
+    const contrasena = localStorage.getItem('contrasena') || '';
+    let direccionPrincipal : string | null = null;
+
+    try {
+      const response = await this.userService.obtenerDireccionPrincipal(usuarioId, nickUsuario, contrasena);
+
+      this.direcciones = response;
+
+      for (let direccion of this.direcciones) {
+        if (direccion.isDireccionPrincipal()) {
+          direccionPrincipal = direccion.nombre;
+          break;
+        }
+      }
+      return direccionPrincipal;
+
+    } catch (error) {
+      console.error('Error real:', error);
+      return null
+    }
+  }
+
+  calcularEdad(fechaNacimiento: string): number {
+    const hoy = new Date();
+    const nacimiento = new Date(fechaNacimiento);
+
+    let edad = hoy.getFullYear() - nacimiento.getFullYear();
+
+    return edad;
   }
 
   onCerrarPopUpOk() {
@@ -30,9 +87,9 @@ export class UserListComponent implements OnInit {
   onCerrarPopUpCancel() {
     this.modoPopup = 'CLOSED';
   }
-  
+
   launchPopup() {
-    
+
     this.modoPopup = 'LAUNCH';
   }
 
