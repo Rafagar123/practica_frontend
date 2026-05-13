6e9c06d101615796ea4a0fc4d4eaf650fab95a86
Rafagar123 <rafagarrido17@gmail.com>
2026-05-13 19:14:50 +0200

    feat: log out button

--- src/app/features/user-list/user-list.component.ts
+++ src/app/features/user-list/user-list.component.ts
@@ -144,6 +144,10 @@ export class UserListComponent implements OnInit {
     }
   }
 
+  volverLogin(){
+    this.router.navigate(['/login']);
+  }
+
   onCerrarPopUpOk() {
     this.modoPopup = 'CLOSED';
   }
